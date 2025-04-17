e94992d3edbd3123992857658e5ba6ce20aa1726
Oliver Joseph Ash <oliverjash@gmail.com>
2025-04-17 08:32:09 +0100

    Format

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 chrome.commands.onCommand.addListener((command) => {
-    if (command === "pause") {
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            const firstTab = tabs[0];
-            if (firstTab !== undefined && firstTab.id !== undefined) {
-                chrome.tabs.executeScript(firstTab.id, {
-                    code: "debugger;",
-                });
-            }
+  if (command === "pause") {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const firstTab = tabs[0];
+      if (firstTab !== undefined && firstTab.id !== undefined) {
+        chrome.tabs.executeScript(firstTab.id, {
+          code: "debugger;",
         });
-    }
+      }
+    });
+  }
 });
